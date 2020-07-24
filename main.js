48d1f172b4ecf84750e204b460867a3facdafb20
ManuDev77 <manuel.dev00@gmail.com>
2020-07-24 19:52:29 +0200

    Agregando primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const grid = new Muuri('.grid', {
+    layout:{
+        rounding: false
+    }
+});
+
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    document.getElementById('grid').classList.add('imagenes-cargadas')
+});
